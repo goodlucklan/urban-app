d4d8064ea23159681179e201355213697991b04d
Luis Enrique Roman Pardo <43737954+goodlucklan@users.noreply.github.com>
2021-11-23 21:30:32 -0500

    Changes

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/styles";
-import { Start } from "./src/screens/Start.screen";
+// import { Start } from "./src/screens/Start.screen";
 import { Principal } from "./src/screens/Principal.screen";
 import { SeleccionFreeStylerProvider } from "./src/providers/SeleccionFreeStyler";
 
--- src/screens/Start.screen.js
+++ src/screens/Start.screen.js
@@ -1,26 +1,30 @@
 import React, { useState, useContext } from "react";
-import { TextInputComponent } from "../components/textInput.component";
-import styled from "styled-components";
+import { WebView } from "react-native-webview";
+import { StyleSheet } from 'react-native';
+import Constants from "expo-constants";
+// import { TextInputComponent } from "../components/textInput.component";
+// import styled from "styled-components";
 import { SafeArea } from "../components/safeArea.component";
-import { Title } from "react-native-paper";
-import { ButtonComponent } from "../components/button.component";
-import { SeleccionContext } from "../providers/SeleccionFreeStyler";
-const Border = styled.View`
-  margin: ${(props) => props.theme.space[2]};
-  flex: 1;
-`;
+// import { Title } from "react-native-paper";
+// import { ButtonComponent } from "../components/button.component";
+// import { SeleccionContext } from "../providers/SeleccionFreeStyler";
+// const Border = styled.View`
+//   margin: ${(props) => props.theme.space[2]};
+//   flex: 1;
+// `;
 export const Start = () => {
-  const [primerMC, setprimerMC] = useState("");
-  const [segundoMC, setsegundoMC] = useState("");
-  const { setcorrecto } = useContext(SeleccionContext);
-  const valuesMC = () => {
-    if (primerMC && segundoMC) {
-      setcorrecto(true);
-    }
-  };
+  // const [primerMC, setprimerMC] = useState("");
+  // const [segundoMC, setsegundoMC] = useState("");
+  // const { setcorrecto } = useContext(SeleccionContext);
+  // const valuesMC = () => {
+  //   if (primerMC && segundoMC) {
+  //     setcorrecto(true);
+  //   }
+  // };
   return (
     <SafeArea>
-      <Border>
+      <WebView style={styles.container} source={{ uri: "https://fmsvotacion.com/vote-window" }} />
+      {/* <Border>
         <Title style={{ textAlign: "center", letterSpacing: 1.5 }}>
           Urban Roosters
         </Title>
@@ -35,7 +39,13 @@ export const Start = () => {
           setChange={(text) => setsegundoMC(text)}
         />
         <ButtonComponent content={"Siguiente"} onPress={() => valuesMC()} />
-      </Border>
+      </Border> */}
     </SafeArea>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
